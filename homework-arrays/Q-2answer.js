390cacbc868f84a4e44fbc30dc3f4a259a358fec
itayLev1 <itaylev.sound@gmail.com>
2018-11-16 14:09:28 +0200

    home-work
    answers for hw array subject

---
+++ homework-arrays/Q-2answer.js
@@ -0,0 +1,17 @@
+// #Question 2
+// ##Take this array let array = [1,2,3,4,5] and copy it using
+// 1. for loop
+// 2. slice function
+
+
+let array = [1,2,3,4,5];
+//let newArray = [];
+//for (i=0; i<array.length; i++){
+ //   newArray[i] = array[i] 
+    
+//}
+//console.log(newArray)
+
+var arrayCopy = array.slice(0, 5)
+console.log(arrayCopy)
+
---
+++ homework-arrays/Q-5answer.js
@@ -0,0 +1,13 @@
+// #Question 5 
+// ##Write a function (function gotMilk(arr):boolean) that checks if milk exists in arr (and return true or false) 
+// e.g. 
+// let items = ['milk', 'bread', 'sugar'];
+// let milkInArray = gotMilk(items) // milkInArray === true
+
+
+let items = ['milk', 'bread', 'sugar'];
+let milkInArray = gotMilk(items)
+
+function gotMilk(){
+    if
+}
