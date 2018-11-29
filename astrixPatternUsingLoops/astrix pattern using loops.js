b0f49f222fad8453efa9d3dce45c0ea7cbe39228
itayLev1 <itaylev.sound@gmail.com>
2018-11-29 23:39:58 +0200

    Create astrix pattern using loops.js

---
+++ astrixPatternUsingLoops/astrix pattern using loops.js
@@ -0,0 +1,14 @@
+const rows = 6
+
+for (let i = 1; rows >= i; i++) {
+    let row = '';
+    for (let k = 1; k <= (rows - i); k++) {
+        row = row + ' ';
+    }
+    for (let j = 1; j <= i * 2; j++) {
+        row = row + '*';
+    }
+
+
+    console.log(row);
+}
